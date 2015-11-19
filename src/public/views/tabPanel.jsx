af99c1fd18e4f775acd6c5493463d4f47575536d
Raymond Ho <rayho@paypal.com>
2015-11-19 14:37:01 +0800

    making the team a variable

--- src/public/views/tabPanel.jsx
+++ src/public/views/tabPanel.jsx
@@ -4,7 +4,8 @@ import { Grid, Row, Col, ProgressBar, Tabs, Tab} from 'react-bootstrap';
 import PageSlider from 'react-page-slider';
 import ReactDom from 'react-dom';
 import Header from './header.jsx';
-const teams = ['Beat Singers', 'Black Angle', 'Dis Band', 'K\'s Angel' ];
+
+const teams = ['Acapalians', 'Black Angle', 'Dis Band', '9th Heaven' ];
 
 class TabPanel extends React.Component {
     constructor() {
@@ -44,30 +45,31 @@ class TabPanel extends React.Component {
     }
 
     render() {
-        const judgeId = this.props.params.judgeId;
-        const data = this.props.data;
+        var that = this;
+        const judgeId = that.props.params.judgeId;
+        const data = that.props.data;
         const results = data[judgeId] || {}; 
 
         const centerText = {
             textAlign:'center'
         }
 
+
         const tabsInstance = (
             <div>
-                <Header judgeId={this.props.params.judgeId}/>
-                <Tabs activeKey={this.state.active} onSelect={this.handleSelect}>
-                    <Tab eventKey={1} title="Talent 1"><Form data={results[teams[0]]} next={this.next} judge={judgeId} team={teams[0]}/></Tab>
-                    <Tab eventKey={2} title="Talent 2"><Form data={results[teams[1]]} next={this.next} judge={judgeId} team={teams[1]}/></Tab>
-                    <Tab eventKey={3} title="Talent 3"><Form data={results[teams[2]]} next={this.next} judge={judgeId} team={teams[2]}/></Tab>
-                    <Tab eventKey={4} title="Talent 4"><Form data={results[teams[3]]} next={this.next} judge={judgeId} team={teams[3]}/></Tab>
+                <Header judgeId={that.props.params.judgeId}/>
+                <Tabs activeKey={that.state.active} onSelect={that.handleSelect}>
+                    {teams.map(function(object, i){
+                        return <Tab eventKey={i+1} title={`Talent ${i+1}`}><Form data={results[teams[i]]} next={that.next} judge={judgeId} team={teams[i]}/></Tab>;
+                    })}                    
                 </Tabs>
-                <PageSlider show={this.state.showSlider}>
+                <PageSlider show={that.state.showSlider}>
                     <div style={centerText}>
                         <h1>Received</h1>
                         <br/>
                         <h1>Next...</h1>
                         <br/>
-                        <h1 style={{color:'red'}}>{this.state.current}</h1>
+                        <h1 style={{color:'red'}}>{that.state.current}</h1>
                     </div>
                 </PageSlider>
             </div>
